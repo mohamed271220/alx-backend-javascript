ab673a1bc4becfe8ba5eafd5ab3516763dea1ea2
mohamed271220 <mohamedaprahym@gmail.com>
2024-10-04 10:14:54 +0300

    classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,33 @@
-export default class HolbertonCourse {
+/* eslint-disable class-methods-use-this */
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this._name = this._validateString(name, 'Name');
+    this._length = this._validateNumber(length, 'Length');
+    this._students = this._validateStudents(students);
   }
 
+  _validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
+    }
+    return value;
+  }
+
+  _validateNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attributeName} must be a number`);
+    }
+    return value;
+  }
+
+  _validateStudents(students) {
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    return students;
+  }
+
+  // Getters
   get name() {
     return this._name;
   }
@@ -17,15 +40,18 @@ export default class HolbertonCourse {
     return this._students;
   }
 
+  // Setters
   set name(newName) {
-    this._name = newName;
+    this._name = this._validateString(newName, 'Name');
   }
 
   set length(newLength) {
-    this._length = newLength;
+    this._length = this._validateNumber(newLength, 'Length');
   }
 
   set students(newStudents) {
-    this._students = newStudents;
+    this._students = this._validateStudents(newStudents);
   }
 }
+
+export default HolbertonCourse;
