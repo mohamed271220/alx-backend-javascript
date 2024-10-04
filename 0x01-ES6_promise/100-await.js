062888b4472da4a2d3fc579e0ebaff0ab3cd0d42
mohamed271220 <mohamedaprahym@gmail.com>
2024-10-04 10:32:31 +0300

    async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,9 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  return { photo, user };
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  }
+  catch (error) {
+    return { photo: null, user: null };
+  }
 }
 
 export default asyncUploadUser;
