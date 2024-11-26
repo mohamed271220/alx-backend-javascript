67c9bc4214d560b4bc4faddf265147613026930c
mohamed271220 <mohamedaprahym@gmail.com>
2024-11-26 14:32:16 +0200

    feat: nodejs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,7 +3,7 @@
  * @param {String} message The message to display.
  */
 const displayMessage = (message) => {
-    console.log(message);
+  console.log(message);
 };
 
 module.exports = displayMessage;
