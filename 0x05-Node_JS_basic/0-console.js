e565ac0d0c5801a0fb222c7305d2b91802681920
mohamed271220 <mohamedaprahym@gmail.com>
2024-11-26 14:30:00 +0200

    feat: nodejs

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} message The message to display.
+ */
+const displayMessage = (message) => {
+    console.log(message);
+};
+
+module.exports = displayMessage;
